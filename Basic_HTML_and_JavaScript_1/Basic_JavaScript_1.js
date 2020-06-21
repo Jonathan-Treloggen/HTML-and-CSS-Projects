1ea7e44b15ac75f55d366238544e0c5ad5bd8937
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-06-21 13:24:12 -0700

    Space management.
    Deleted individual files and added the whole folder instead plus, an introduction of a aditional folder.

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Will you kiss an Irish man?");
+function My_First_Function() {
+		var String = "Lucky charms added to your inventory!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
