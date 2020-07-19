7879829b0c5dd79b93a5b401511e19a7fe9251f9
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-07-19 10:56:22 -0700

    Update Basic_JavaScript_1.js

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,7 +1,2 @@
-alert("Will you kiss an Irish man?");
-function My_First_Function() {
-		var String = "Lucky charms added to your inventory!";
-		var result = String.fontcolor("green");
-		document.getElementById("Irish").innerHTML =
-		result;
-	}
+window.alert("Why Hello.")
+document.write("Hello, Hello!");
